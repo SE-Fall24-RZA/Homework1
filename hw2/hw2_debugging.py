68ff163488e3613520e19115838939de5545e0f0
Anuraag Jajoo <84722161+anurgj@users.noreply.github.com>
2024-09-10 18:00:27 +0000

    Added trace files & fixed code

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -1,37 +1,35 @@
 import rand
 
 
-def mergeSort(arr):
-    if (len(arr) == 1):
+def merge_sort(arr):
+    if len(arr) == 1:
         return arr
-
     half = len(arr) // 2
-
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
 
 def recombine(leftArr, rightArr):
     leftIndex = 0
     rightIndex = 0
     mergeArr = [None] * (len(leftArr) + len(rightArr))
+
     while leftIndex < len(leftArr) and rightIndex < len(rightArr):
         if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
             mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        else:
             leftIndex += 1
+        else:
             mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-
+            rightIndex += 1
     for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+        mergeArr[leftIndex + i] = rightArr[i]
 
     for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+        mergeArr[i + rightIndex] = leftArr[i]
 
     return mergeArr
 
 
 arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
-
+print(arr)
+arr_out = merge_sort(arr)
 print(arr_out)
