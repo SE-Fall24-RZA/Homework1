a6c271f410ba0e2da38c4f4feb09e6c87844b33e
robbiethomas1101 <165846791+robbiethomas1101@users.noreply.github.com>
2024-10-06 17:09:55 +0000

    Fixes to task2

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -1,3 +1,4 @@
+# Part A:
 grep -c "sample" dataset1/file* | 
 grep -E ":[1-9]+[0-9]*$" | 
 cut -d: -f1 | 
@@ -5,8 +6,10 @@ xargs grep -o "CSC510" |
 uniq -c | 
 grep -E -v "^\s*[1-2] dataset1/file" | 
 cut -d: -f1 | 
+# Part B:
 gawk '{system("ls -l "$2 "| xargs echo "$1 ) }' | 
 sort -k1,1nr -k6,6nr | 
 gawk '{print $10}' | 
 cut -d/ -f2 |
+# Part C:
 sed 's/file/filtered/g'
