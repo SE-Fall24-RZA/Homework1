22662a2e30b5f2c265df6f808b2e8a8adeb9fd60
robbiethomas1101 <165846791+robbiethomas1101@users.noreply.github.com>
2024-10-06 17:06:22 +0000

    Added hw4 files and completed task2

---
+++ hw4/task2.sh
@@ -0,0 +1,12 @@
+grep -c "sample" dataset1/file* | 
+grep -E ":[1-9]+[0-9]*$" | 
+cut -d: -f1 | 
+xargs grep -o "CSC510" | 
+uniq -c | 
+grep -E -v "^\s*[1-2] dataset1/file" | 
+cut -d: -f1 | 
+gawk '{system("ls -l "$2 "| xargs echo "$1 ) }' | 
+sort -k1,1nr -k6,6nr | 
+gawk '{print $10}' | 
+cut -d/ -f2 |
+sed 's/file/filtered/g'
