8718f841f43e553b749c0302e80e25f32c5a8eb2
Anuraag Jajoo <84722161+anurgj@users.noreply.github.com>
2024-10-10 16:55:26 +0000

    Added "Average age: "

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,4 +1,4 @@
 grep -E '^([^,]*,[^,]*,2,)' titanic.csv | 
 grep -E ',S\s' | 
 sed 's/female/F/; s/male/M/'| 
-gawk -F, 'NR > 0 && $7 != "" { sum += $7; count++; print $0} END { if (count > 0) print sum / count; else print "No valid ages" }' > outputTask3.txt
+gawk -F, 'NR > 0 && $7 != "" { sum += $7; count++; print $0} END { if (count > 0) print "Average age: ", sum / count; else print "No valid ages" }' > outputTask3.txt
